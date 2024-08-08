33f56de3ea2dae55b6dfa2329ce93b10e69f795f
hamzakhanmpn <163988551+hamzakhanmpn@users.noreply.github.com>
2024-08-08 16:33:49 +0100

    get all materials api

--- backend/node/controllers/materialController.js
+++ backend/node/controllers/materialController.js
@@ -1,4 +1,23 @@
+const multer = require('multer');
+const mysql = require('mysql2');
+
+// MySQL connection setup
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'invntzA!#',
+    database: 'sustainabilitydb2'
+});
+
 const getAllMaterials = (req, res) => {
+    const query = 'Select * FROM materials';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error storing image:', err);
+            return res.status(500).json({ error: 'Error storing image' });
+        }
+        res.status(201).json({ message: 'Image uploaded successfully', results });
+    });
 
 }
 
