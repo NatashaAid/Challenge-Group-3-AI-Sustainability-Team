89fe1b587d6ba5b63015b0484539f518e3da9e68
hamzakhanmpn <163988551+hamzakhanmpn@users.noreply.github.com>
2024-08-08 16:38:16 +0100

    sustainable manufacturers & suppliers

--- backend/node/controllers/sustainableManufacturersController.js
+++ backend/node/controllers/sustainableManufacturersController.js
@@ -1,4 +1,22 @@
+const mysql = require('mysql2');
+
+// MySQL connection setup
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'invntzA!#',
+    database: 'sustainabilitydb2'
+});
+
 const getSustainableManufacturers = (req, res) => {
+    const query = 'Select * FROM materials';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error storing image:', err);
+            return res.status(500).json({ error: 'Error storing image' });
+        }
+        res.status(201).json({ message: 'Image uploaded successfully', results });
+    });
 
 }
 
--- backend/node/controllers/sustainableSupplierController.js
+++ backend/node/controllers/sustainableSupplierController.js
@@ -1,4 +1,23 @@
+const mysql = require('mysql2');
+
+// MySQL connection setup
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'invntzA!#',
+    database: 'sustainabilitydb2'
+});
+
+
 const getSustainableSuppliers = (req, res) => {
+    const query = 'Select * FROM suppliers';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error storing image:', err);
+            return res.status(500).json({ error: 'Error storing image' });
+        }
+        res.status(201).json({ message: 'Image uploaded successfully', results });
+    });
 
 }
 
